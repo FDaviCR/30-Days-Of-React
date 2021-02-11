49855ad52f95c7e405657b4bc89e7e6e8472c093
Davi Camilo <davicamiloribeiro@outlook.com>
2021-02-11 00:30:41 -0300

    Array parte 2

---
+++ Day 1/3/preencher.js
@@ -0,0 +1,2 @@
+const eightXvalues = Array(8).fill('X');
+console.log(eightXvalues);
---
+++ Day 1/3/procurarItem.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.indexOf(5)) 
+console.log(numbers.indexOf(0)) 
+console.log(numbers.indexOf(1)) 
+console.log(numbers.indexOf(6)) 
