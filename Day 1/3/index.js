78a913824da747985fc64f0c620a204ad993c3b6
Davi Camilo <davicamiloribeiro@outlook.com>
2021-02-10 23:52:45 -0300

    Array parte 1

---
+++ Day 1/3/index.js
@@ -0,0 +1,13 @@
+var arr1 = Array( );
+var arr2 = [];
+
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+let txt = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words);
