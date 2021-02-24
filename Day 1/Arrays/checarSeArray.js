fb9edc5cde377a788973e82b8f600402f52e6db0
Davi Camilo <davicamiloribeiro@outlook.com>
2021-02-24 01:22:32 -0300

    Arrays parte 3

---
+++ Day 1/Arrays/checarSeArray.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5]
+console.log(Array.isArray(numbers)) 
+
+const number = 100
+console.log(Array.isArray(number)) 
---
+++ Day 1/Arrays/ultimoIndice.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+console.log(numbers.lastIndexOf(2)) 
+console.log(numbers.lastIndexOf(0)) 
+console.log(numbers.lastIndexOf(1)) 
+console.log(numbers.lastIndexOf(4)) 
+console.log(numbers.lastIndexOf(6)) 
